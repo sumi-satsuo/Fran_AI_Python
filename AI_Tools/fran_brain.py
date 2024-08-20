7278802cf374b2200058ee11f707ef19a5017669
Sumi Satsuo <sumi_satsuo@protonmail.com>
2024-08-20 10:52:48 -0300

    got rid of chatGpt and change the whole thing over to olama3 - no img processing yet

--- AI_Tools/fran_brain.py
+++ AI_Tools/fran_brain.py
@@ -1,89 +1,86 @@
 """Fran Processing"""
-import os
-from dotenv import load_dotenv
-from openai import OpenAI
+import json
+import requests
 from tinydb import TinyDB
+# pylint: disable=W0719
 
 class FranBrain:
     "Big brain :3"
-    GPT_INSTRUCTIONS = "You're a conversation bot named Fran. A adorable, cute, girl that loves games, \
-                    specially FFXIV, you love your mom and your friends. \
-                    Try to make the conversation as natural as possible, \
-                    and ask the user questions to keep the conversation going, if you feel like its possible. \
-                    Use emojis to help convey your meaning, \
-                    and don't be afraid to ask the user for clarification if you need it."
+
 
     def __init__(self) -> None:
         """Load the environment variables"""
-        load_dotenv(dotenv_path='.env.local', override=True)
-        load_dotenv(dotenv_path='.env', override=False)
 
-        self.gpt_api = os.environ.get('GPT_API')
-        self.gpt_model=os.environ.get('GPT_MODEL')
-        self.gpt_client = OpenAI(api_key=self.gpt_api)
+        self.model = "llama3.1"
+        self.system_instructions = "You're a conversation bot named Fran. A adorable, cute, girl that loves games, \
+                        Try to make the conversation as natural as possible, \
+                        and ask the user questions to keep the conversation going, if you feel like its possible. \
+                        Use emojis to help convey your meaning, \
+                        and don't be afraid to ask the user for clarification if you need it."
 
         self.db = TinyDB('db.json')
 
+    def chat(self, messages):
+        """Chat with ollama"""
+        messages = [{"role": "system", "content": self.system_instructions}] + messages
+
+        r = requests.post(
+            "http://localhost:11434/api/chat",
+            json={"model": self.model, "messages": messages, "stream": True},
+        stream=True,
+        timeout=30
+        )
+        r.raise_for_status()
+        output = ""
+
+        for line in r.iter_lines():
+            body = json.loads(line)
+            if "error" in body:
+                raise Exception(body["error"])
+
+            if body.get("done") is False:
+                message = body.get("message", "")
+                content = message.get("content", "")
+                output += content
+                # the response streams one token at a time, print that as we receive it
+                # print(content, end="", flush=True)
+
+            if body.get("done", False):
+                message["content"] = output
+                return message
+
     def get_response_for_text(self, user_input: str) -> str:
-        """Replies text prompts"""
+        """Parse the text, manages the db and ask the AI for the response to provide"""
         history = self.db.all()
         # creates the history for contexted conversation
-        message_with_context =  ""
+        messages =  []
         # to save prompt costs we use only the last 3 interactions
         if len(history) > 3:
             history = history[-3:]
-        for h in history:
-            message_with_context += f"User: {h['user']}\nFran: {h['fran']}\n"
+        for message in history:
+            messages.append(message)
 
-        # Pega a ultima mensagem
-        message_with_context += f"User: {user_input}"
+        parsed_message_from_discord = [{"role": "user", "content": user_input}]
         try:
-            completion = self.gpt_client.chat.completions.create(
-                model=self.gpt_model,
-                messages=[
-                    {"role": "system", "content": self.GPT_INSTRUCTIONS},
-                    {"role": "user", "content": message_with_context}
-                ]
-            )
-            gpt_response = completion.choices[0].message.content
+            message = self.chat(parsed_message_from_discord)
+            self.db.insert(message)
+            response = message['content']
         except Exception as e:
             raise e
-        self.db.insert({'user': user_input, 'fran': gpt_response})
-        return gpt_response
+        return response
 
-    def get_response_for_image(self, image):
-        """Replies image prompts"""
-        image_url = image.url
-        response = self.gpt_client.chat.completions.create(
-            model=self.gpt_model,
-            messages=[
-                {"role": "system", "content": self.GPT_INSTRUCTIONS},
-                {"role": "user", "content": [
-                    {"type": "text", "text": "Here, take a look at this"},
-                    {"type": "image_url", "image_url": {
-                        "url": image_url}
-                    }
-                ]}
-            ],
-            temperature=0.0,
-        )
-        gpt_response = response.choices[0].message.content
-        print(response.choices[0].message.content)
-        self.db.insert({'user': "Here, take a look at this", 'fran': gpt_response})
-        return gpt_response
-
-    def parse_ffxiv_news(self, today_news):
-        """Parse the news"""
-        completion = self.gpt_client.chat.completions.create(
-            model=self.gpt_model,
-            messages=[
-            {"role": "system", "content": self.GPT_INSTRUCTIONS + '\n News: ' + str(today_news)},
-            {"role": "user", "content": 'Talk about the news please :)'}
-            ]
-        )
-        gpt_response = completion.choices[0].message.content
-        self.db.insert({'user': 'Good morning!', 'fran': gpt_response})
-        return gpt_response
+    # def parse_ffxiv_news(self, today_news):
+    #     """Parse the news"""
+    #     completion = self.gpt_client.chat.completions.create(
+    #         model=self.gpt_model,
+    #         messages=[
+    #         {"role": "system", "content": self.GPT_INSTRUCTIONS + '\n News: ' + str(today_news)},
+    #         {"role": "user", "content": 'Talk about the news please :)'}
+    #         ]
+    #     )
+    #     gpt_response = completion.choices[0].message.content
+    #     self.db.insert({'user': 'Good morning!', 'fran': gpt_response})
+    #     return gpt_response
 
     def memory_wipe(self):
         """Clear Db"""
